08037c4794f7c41badfb6ea5925b3e399b7c7df0
Handroo <hanandre@usc.edu>
2015-06-07 16:23:09 -0700

    First commit

---
+++ Instabook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instabook
+//
+//  Created by Andrew Han on 6/7/15.
+//  Copyright (c) 2015 Andrew Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instabook/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Instabook
+//
+//  Created by Andrew Han on 6/7/15.
+//  Copyright (c) 2015 Andrew Han. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
